7ec562619e6e6dd696515e82494ff857e96f80cc
Alexey Litovchenko <alex.lit@outlook.com>
2017-11-07 23:44:07 +0300

    .

--- service-worker.js
+++ service-worker.js
@@ -2,25 +2,25 @@
 // some other post)
 const cacheName = 'v1::static';
 
-self.addEventListener('install', e => {
-    // once the SW is installed, go ahead and fetch the resources
-    // to make this work offline
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(['/']).then(() => self.skipWaiting());
-        })
-    );
+window.self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => cache.addAll(['/']).then(() => self.skipWaiting()))
+  );
 });
 
 // when the browser fetches a url, either response with
 // the cached object or go ahead and fetch the actual url
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        // ensure we check the *right* cache to match against
-        caches.open(cacheName).then(cache => {
-            return cache.match(event.request).then(res => {
-                return res || fetch(event.request);
-            });
-        })
-    );
+window.self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches
+      .open(cacheName)
+      .then(cache =>
+        cache.match(event.request).then(res => res || fetch(event.request))
+      )
+  );
 });
